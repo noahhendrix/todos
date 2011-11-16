c9bd07800eab3bdce935dea280631314b87e6a31
Noah Hendrix <noahhendrix@gmail.com>
2011-11-15 18:25:50 -0600

    Todo List Controller w/ create action

--- apps/todos/todos.js
+++ apps/todos/todos.js
@@ -18,3 +18,16 @@ SC.ready(function() {
     templateName: 'todos'
   });
 });
+
+// Controllers
+  Todos.todoListController = SC.ArrayController.create({
+    // Initialize the array controller with an empty array.
+    content: [],
+
+    // Creates a new todo with the passed title, then adds it
+    // to the array.
+    createTodo: function(title) {
+      var todo = Todos.Todo.create({ title: title });
+      this.pushObject(todo);
+    }
+  });
