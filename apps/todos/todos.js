cdd98eb053f5edb29fd5bbcbc1fe87d8b353a27d
Noah Hendrix <noahhendrix@gmail.com>
2011-11-27 19:49:04 -0600

    Remove fixtures

--- apps/todos/todos.js
+++ apps/todos/todos.js
@@ -4,8 +4,8 @@
 // ==========================================================================
 /*globals Todos */
 
-Todos = SC.Application.create({
-  store: SC.Store.create().from(SC.Record.fixtures)
+var Todos = SC.Application.create({
+  store: SC.Store.create()
 });
 
 // Models
@@ -13,20 +13,6 @@ Todos = SC.Application.create({
     title: SC.Record.attr(String),
     isDone: SC.Record.attr(Boolean, { defaultValue: NO })
   });
-  
-  Todos.Todo.FIXTURES = [
-      { "guid": "todo-1",
-        "title": "Build my first SproutCore app",
-        "isDone": false },
-
-      { "guid": "todo-2",
-        "title": "Build a really awesome SproutCore app",
-        "isDone": false },
-
-      { "guid": "todo-3",
-        "title": "Next, the world!",
-        "isDone": false }
-  ];
 
 // Views
   Todos.CreateTodoView = SC.TextField.extend({
